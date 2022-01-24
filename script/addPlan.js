68e6d648e3f2830299cfbbf1eb04e3269497b98c
Clint <40567645+cakasten@users.noreply.github.com>
2022-01-24 17:45:38 +0100

    use js to input times for game plan selctions

---
+++ script/addPlan.js
@@ -0,0 +1,34 @@
+
+
+for (let i = 0; i < 24; i++) {
+    let sel = document.getElementById('startTime')
+    let optionTime = document.createElement('option');
+    if (i === 0) {
+        optionTime.innerHTML = '12:00 PM';
+    } else if (i < 12) {
+        optionTime.innerHTML = i + ':00 PM';
+    } else if (i === 12) {
+        optionTime.innerHTML = i + ':00 AM';
+    }else {
+        optionTime.innerHTML = (i - 12) + ':00 AM';
+    }
+    optionTime.value = 'hour' + i;
+    sel.add(optionTime);
+
+}
+for (let i = 0; i < 24; i++) {
+    let sel = document.getElementById('endTime')
+    let optionTime = document.createElement('option');
+    if (i === 0) {
+        optionTime.innerHTML = '12:00 PM';
+    } else if (i < 12) {
+        optionTime.innerHTML = i + ':00 PM';
+    } else if (i === 12) {
+        optionTime.innerHTML = i + ':00 AM';
+    }else {
+        optionTime.innerHTML = (i - 12) + ':00 AM';
+    }
+    optionTime.value = 'hour' + i;
+    sel.add(optionTime);
+
+}
